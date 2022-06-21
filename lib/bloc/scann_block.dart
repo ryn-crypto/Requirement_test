6cc1f03be149ac7a99af4fa2eb7560cc528bb0b6
Riyan First Tiyanto <riyanfirst13@gmail.com>
2022-06-21 18:33:29 +0700

    memeprbaiki scann bloc

--- lib/bloc/scann_block.dart
+++ lib/bloc/scann_block.dart
@@ -4,8 +4,8 @@ import 'package:abon/helpers/api.dart';
 import 'package:abon/model/scann.dart';
 
 class ScannBloc {
-  static Future<Scann> login({int? id, String? scanurl}) async {
-    var body = {"id": id};
+  static Future<Scann> scann({int? nik, int? id, String? scanurl}) async {
+    var body = {"nik": nik, "id": id};
     var response = await Api().post(scanurl, body);
     var jsonObj = json.decode(response.body);
     return Scann.formJson(jsonObj);
